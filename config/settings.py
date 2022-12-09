58bd3faa70a2ba95a706ab077f3cc3bc07948b07
Chicken-iniad <s1f102200075@iniad.org>
2022-12-09 17:36:27 +0900

    file_initialized

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bc1v4p0d-($#2n5*l7amh*!0uvozkpzev232=rg8kn$!$xay1o'
+SECRET_KEY = 'django-insecure-gqgwlp2lh7r!n^pbl4h&bniq89zu5&44@c&ec_wpyrxd@nit&%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'foods'
+    'gacha'
 ]
 
 MIDDLEWARE = [
