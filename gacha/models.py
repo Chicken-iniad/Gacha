f3a3fc83fab6d7d8b9e82e1e09be76cd7b4c7b08
Chicken-iniad <s1f102200075@iniad.org>
2022-12-23 17:22:18 +0900

    取得モンスターを格納するmodelを定義
    実際に格納するプログラムは未実装

--- gacha/models.py
+++ gacha/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+class Monster(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    monsters = ArrayField(models.CharField(max_length=100))
